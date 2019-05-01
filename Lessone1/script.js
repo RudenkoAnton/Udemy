9aa5e2fd8576c55e0ea7b9d1c473c85f84d4a10c
Anton <12-ist-4-rudenko.a.o@mail.ru>
2019-05-01 22:34:35 +0300

    Second commit.

--- Lessone1/script.js
+++ Lessone1/script.js
@@ -4,16 +4,19 @@ var time = prompt("Введите дату в формате YYYY-MM-DD","");
 var appData = {
     budget : money,
     timeData : time,
-    expenses : {
-        itemOfExpenditure : amountOfExpenses
-    },
-    optionalExpenses : undefined,
-    income : undefined,
+    expenses : {},
+    optionalExpenses : {},
+    income : [],
     saving : false
 };
 
 var itemOfExpenditure = prompt("Введите обязательную статью расходов в этом месяце","");
 var amountOfExpenses = prompt("Во сколько обойдется","");
+var itemOfExpenditure1 = prompt("Введите обязательную статью расходов в этом месяце","");
+var amountOfExpenses1 = prompt("Во сколько обойдется","");
 
-alert(+money / 30);
+appData.expenses.itemOfExpenditure = amountOfExpenses;
+appData.expenses.itemOfExpenditure1 = amountOfExpenses1;
+
+alert(+appData.budget / 30);
 
